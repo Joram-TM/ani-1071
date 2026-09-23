870953af8a38d3bbfe828267f95b76682271d7a8
Aimé Nasser <joram030309@gmail.com>
2026-09-22 21:49:29 -0700

    premiere_tentative

---
+++ chapitre-01/exo4-le_code_de_sortie/c1-exo4_main.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main(){
+
+    printf("Hello");
+
+    return 3;
+}
+//C:\Users\New User\Desktop\COURS TEUGUIA\ani-1071\chapitre-01\exo4-le_code_de_sortie>echo %ERRORLEVEL%
+//3
+//pour un outil qui enchaine des programmes, ce nombre permet de verifier que la compilation se passe bien
