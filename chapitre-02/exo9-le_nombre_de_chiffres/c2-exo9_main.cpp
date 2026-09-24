d598a84cc4a11836362cc8d05a684520810c49ba
Aimé Nasser <joram030309@gmail.com>
2026-09-24 16:54:38 -0700

    tentative_un

---
+++ chapitre-02/exo9-le_nombre_de_chiffres/c2-exo9_main.cpp
@@ -0,0 +1,16 @@
+#include <cstdio>
+
+int main (){
+ int nombre;
+ int chiffre;
+    printf ("entrer votre nombre:");
+    scanf ("%d",&nombre);
+    chiffre=0;
+    while (nombre>0){
+        nombre=nombre/10;
+        chiffre=chiffre+1;
+    }
+    printf ("ce nombre comporte %d chiffres" , chiffre);
+    printf ("ok");
+    return 0;
+}
