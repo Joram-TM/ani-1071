186b900243764fcea884a2844b890736b65b46c7
Aimé Nasser <joram030309@gmail.com>
2026-09-25 06:55:17 -0700

    tentative_deux

--- chapitre-02/exo2-somme_et_moyenne/c2-exo2_main.cpp
+++ chapitre-02/exo2-somme_et_moyenne/c2-exo2_main.cpp
@@ -5,15 +5,17 @@ int main(){
     int i=1;
     int nombre; 
     double moyenne=0;
-    int somme=0;
+    double somme=0;
 
-    for (i=1; i==5; i++){
+    for (i=1; i<=5; i++){
 
         printf ("entrer un nombre:");
         scanf ("%d", &nombre);
         somme=somme+nombre;
     }
-    printf ("la somme vaut %d", somme);
+    printf ("la somme vaut %f", somme);
     moyenne=somme/5 ;
+    printf (" la moyenne vaut %f", moyenne);
+    
     return 0;
 }
