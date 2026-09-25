a13877a82115246eb6816a942586c7bf4debaf7b
Aimé Nasser <joram030309@gmail.com>
2026-09-24 18:23:15 -0700

    triangle

---
+++ chapitre-02/exo14-le-triangle/c2-exo14_main.cpp
@@ -0,0 +1,12 @@
+#include <cstdio>
+
+int main (){
+
+    printf ("   *\n");
+    printf ("  ***\n");
+    printf (" *****\n");
+    printf ("*******\n");
+    scanf ("o");
+    
+    return 0;
+}
---
+++ chapitre-02/exo14-le-triangle/c2-exo14_reponse.md
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+int main (){
+
+    printf ("   *");
+    printf ("  ***");
+    printf (" *****");
+    printf ("*******");
+    
+    return 0;
+}
